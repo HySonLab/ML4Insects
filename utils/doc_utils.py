1b9095bcf40465870c4830216490d09b5a6739f3
DINH Quang Dung (Đinh Quang Dũng) <dqdung999@gmail.com>
2024-02-22 10:54:25 +0100

    Update doc_utils.py

--- utils/doc_utils.py
+++ utils/doc_utils.py
@@ -2,24 +2,6 @@ import numpy as np
 import os 
 wd = os.getcwd()
 
-def get_dataset_group(name):
-    if name == 'BCOA':
-        return ['BCOA-Wheat', 'day2run2', 'reutday3', 'reutwheat', 'reutwheat2', 
-                'reutwheatday', 'reutwheatrun3', 'wheatday2run2', 'wheatday2run3', 'wheatrun']
-    elif name == 'zt':
-        return ['0zt','8zt','16zt']
-    elif name == 'cannabis':
-        return ['non-viruliferous-hemp', 'non-viruliferous-potato','viruliferous-hemp', 'viruliferous-potato']
-    elif name == 'Anuradha': # BCOA maybe?
-        return ['Anuradha16', 'Anuradha27', 'Anuradha42', 'Anuradha57', 'Anuradha75', 'Anuradha84', 'Anuradha107']
-    elif name == 'wheatrnai': # BCOA maybe too?
-        return ['wheatrnai']
-    elif name == 'sorghum': #sorghum
-        return ['sorghumaphid']
-    elif name == 'ArabidopsisGPA':
-        return ['ArabidopsisGPA']
-
-# ============================= Names of train/test recordings =============================
 def get_filename(name):
     list_files = os.listdir(os.path.join(wd,'data',name))
     unique = []
